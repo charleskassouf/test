eb894912e855071bc6b9eb26f230d3f47dcbab90
charleskassouf <charles.kassouf11@gmail.com>
2018-02-26 12:04:40 +0200

    first commit for test file

---
+++ config.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = {
+  serviceName: "cart",
+  serviceVersion: 1,
+  servicePort: 6531,
+  serviceGroup: "SOAJS ",
+  extKeyRequired: true,
+  oauth: false,
+  urac: false,
+  type: "service",
+  prerequisites: {
+    cpu: '',
+    memory: ''
+  },
+  "errors": {},
+  "schema": {}
+};
---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+var soajs = require('soajs');
+var config = require('./config.js');
+var BLModule = require("./lib/index");
+
+BLModule.getCart(function (error, result) {
+  console.log( result ); // ToDelete #2del
+});
